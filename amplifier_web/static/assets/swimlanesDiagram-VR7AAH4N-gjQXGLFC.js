dd1b4fd21949443d582c0992e503cb90d7a1fc25
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 15:10:48 -0700

    Build assets for native provider controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-gjQXGLFC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-EDVQNSb1.js";import{_ as a}from"./mermaid.core-Bgj5uKjV.js";import"./chunk-5VM5RSS4-D5Ty3cgx.js";import"./chunk-XXDRQBXY-DQxFqJzm.js";import"./chunk-POPQ4Y6H-Bj8ynmKk.js";import"./chunk-F27PBJKO-GdjckfT7.js";import"./app-BbsavlI8.js";import"./client-BPXDbY79.js";import"./channel-BQWdInYN.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
