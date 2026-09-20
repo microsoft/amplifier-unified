411f1191399d0c4db7ac5c55b02a9640097af369
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 11:28:41 -0700

    Keep manual model entry available after a failed refresh

--- amplifier_web/static/assets/channel-BtT07mby.js
+++
@@ -1,1 +0,0 @@
-import{ah as o,ai as n}from"./mermaid.core-aP33nc0M.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DYe37Dhu.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-_FXBQ6lh.js";import{_ as a}from"./mermaid.core-rG8YdoMX.js";import"./chunk-5VM5RSS4-AFJdoHbL.js";import"./chunk-XXDRQBXY-DJdzG4JQ.js";import"./chunk-POPQ4Y6H-H6kP01P9.js";import"./chunk-F27PBJKO-CqebwA6h.js";import"./app-xjLoJ_JY.js";import"./client-agr-qz4F.js";import"./channel-B7fa5GWj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
