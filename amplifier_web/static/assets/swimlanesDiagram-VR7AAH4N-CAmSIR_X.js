ef684b99b7c39fe72727b843f97f28e830428665
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 13:59:06 -0700

    Build task coordination browser assets

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CAmSIR_X.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DoBm-iUV.js";import{_ as a}from"./mermaid.core-B9kaaXZi.js";import"./chunk-5VM5RSS4-DfsjopY_.js";import"./chunk-XXDRQBXY-VbFljGHN.js";import"./chunk-POPQ4Y6H-DaSSDsn_.js";import"./chunk-F27PBJKO-B3Jgqw6W.js";import"./app-Bio0Oirw.js";import"./client-BPXDbY79.js";import"./channel-CKhezjuX.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
