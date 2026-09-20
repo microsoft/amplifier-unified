8fcd57dba4f388fc0491c1f0992be68f2d5d0827
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 13:29:17 -0700

    Build operation panel assets

---
+++ amplifier_web/static/assets/channel-DYoGCCJ7.js
@@ -0,0 +1,1 @@
+import{ah as o,ai as n}from"./mermaid.core-B3Q9wv4k.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CEhM2Hxs.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BBPfTn-4.js";import{_ as a}from"./mermaid.core-B3Q9wv4k.js";import"./chunk-5VM5RSS4-HevOtVus.js";import"./chunk-XXDRQBXY-0wUyAzui.js";import"./chunk-POPQ4Y6H-B9b3doSv.js";import"./chunk-F27PBJKO-Bu8kn1TO.js";import"./app-BNo-1dfw.js";import"./client-BPXDbY79.js";import"./channel-DYoGCCJ7.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
