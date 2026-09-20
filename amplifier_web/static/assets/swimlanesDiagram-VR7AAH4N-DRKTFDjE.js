e69575ddc6a663ed85a49bbe3be98a3d4314af8f
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 16:00:48 -0700

    Build frontend with stable approval receipt selectors

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DRKTFDjE.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-Cs13EQ46.js";import{_ as a}from"./mermaid.core-DH4vNRCj.js";import"./chunk-5VM5RSS4-DTAoJ18K.js";import"./chunk-XXDRQBXY-DouoqbiY.js";import"./chunk-POPQ4Y6H-aM1pOrBR.js";import"./chunk-F27PBJKO-CP8UuBvz.js";import"./app-CJS5gpKM.js";import"./client-BPXDbY79.js";import"./channel-pzplCsre.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DoVAo3Mz.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CoDdJYin.js";import{_ as a}from"./mermaid.core-q0I3TLsn.js";import"./chunk-5VM5RSS4-lgQk2pSg.js";import"./chunk-XXDRQBXY-DjFFmrDy.js";import"./chunk-POPQ4Y6H-BdV_f8rY.js";import"./chunk-F27PBJKO-CoBP9QsS.js";import"./app-9WXuoujk.js";import"./client-BPXDbY79.js";import"./channel-bZz7ZgFE.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
