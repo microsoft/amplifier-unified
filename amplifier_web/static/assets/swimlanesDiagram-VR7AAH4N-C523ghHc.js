f7eafa62f9643747e4fba8ee75ce73465d4e2083
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 07:46:11 -0700

    Refresh packaged frontend identity for 0.12.1

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-C523ghHc.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DkpXdZx-.js";import{_ as a}from"./mermaid.core-DVujbxCb.js";import"./chunk-5VM5RSS4-HXiWo31B.js";import"./chunk-XXDRQBXY-Bkz20CDB.js";import"./chunk-POPQ4Y6H-iOCDWp-I.js";import"./chunk-F27PBJKO-35qY8dvC.js";import"./app-2-S1A1lu.js";import"./client-agr-qz4F.js";import"./channel-DGq-Ltv0.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
