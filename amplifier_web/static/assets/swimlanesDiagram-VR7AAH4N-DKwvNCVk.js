6fe8b7c450513d8d5f56f3107e5592899ee4a111
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 16:51:13 -0700

    Rebuild voice native foreground controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DKwvNCVk.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-5qHMBNcJ.js";import{_ as a}from"./mermaid.core-CrqFV7Xh.js";import"./chunk-5VM5RSS4-YDb0_r4Y.js";import"./chunk-XXDRQBXY-Z5Yaf1pE.js";import"./chunk-POPQ4Y6H-DIsSmQcO.js";import"./chunk-F27PBJKO-CCKK5iOi.js";import"./app-BPUYmeMs.js";import"./client-BPXDbY79.js";import"./channel-f0_wp2ku.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
