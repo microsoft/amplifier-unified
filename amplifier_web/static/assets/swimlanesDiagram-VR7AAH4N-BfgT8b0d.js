010434cb68fcc61e1b92d804cbf5593211102dab
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 15:02:25 -0700

    Build task capacity controls for packaged frontend

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BfgT8b0d.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BEGfBK_b.js";import{_ as a}from"./mermaid.core-CRO7YdfA.js";import"./chunk-5VM5RSS4-CT8RDgj8.js";import"./chunk-XXDRQBXY-Ex979b2K.js";import"./chunk-POPQ4Y6H-B1ECAKP6.js";import"./chunk-F27PBJKO-30HN3Jvu.js";import"./app-6-I2-tIo.js";import"./client-BPXDbY79.js";import"./channel-BnA217Gq.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
