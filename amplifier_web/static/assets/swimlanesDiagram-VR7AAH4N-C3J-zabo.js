69bc57f83a43c8bfd07a3507f60292b63ab30a31
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 11:38:28 -0700

    Release stable navigation as 0.17.0 with packaged frontend and browser checks

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-C3J-zabo.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-BkoJv7Yz.js";import{_ as a}from"./mermaid.core-CxOnTBYH.js";import"./chunk-5VM5RSS4-BXaAEFaP.js";import"./chunk-XXDRQBXY-hNE3nbTE.js";import"./chunk-POPQ4Y6H-D0RnZVU-.js";import"./chunk-F27PBJKO-CrakhFoh.js";import"./app-BYG4aRyD.js";import"./client-agr-qz4F.js";import"./channel-CbzjkRn8.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DBGD4Gat.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-MvsIUVnt.js";import{_ as a}from"./mermaid.core-8nEH0bt5.js";import"./chunk-5VM5RSS4-C85oA73j.js";import"./chunk-XXDRQBXY-UGQVYE1T.js";import"./chunk-POPQ4Y6H-BCrT4XSp.js";import"./chunk-F27PBJKO-BVGhY7-x.js";import"./app-Dyu100GI.js";import"./client-BPXDbY79.js";import"./channel-DWGWd5lj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
