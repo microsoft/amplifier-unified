3cf354c8c639f10d7636529678cad2b8e8df816b
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 13:31:21 -0700

    Rebuild browser assets for saved task controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-U9DEj_FB.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-ElEn8BST.js";import{_ as a}from"./mermaid.core-B_0O3Jg4.js";import"./chunk-5VM5RSS4-BtKYA52m.js";import"./chunk-XXDRQBXY-8u7F9-8x.js";import"./chunk-POPQ4Y6H-MzzbGVlX.js";import"./chunk-F27PBJKO-BpzOp54g.js";import"./app-DWotX5FW.js";import"./client-BPXDbY79.js";import"./channel-4hdCNn5O.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
