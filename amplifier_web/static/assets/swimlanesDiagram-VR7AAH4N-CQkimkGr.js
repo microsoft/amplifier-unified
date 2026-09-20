b7053f63ea4998417ac96eebfd32d5ab48170f9f
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 13:29:58 -0700

    Build conversation library frontend assets

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CQkimkGr.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-Bwf7DFQw.js";import{_ as a}from"./mermaid.core-CgOkB5ap.js";import"./chunk-5VM5RSS4-DJRxHmMf.js";import"./chunk-XXDRQBXY-KNx2SeSn.js";import"./chunk-POPQ4Y6H-DoeELg6a.js";import"./chunk-F27PBJKO-DyAlpbCl.js";import"./app-Cdze8zzQ.js";import"./client-BPXDbY79.js";import"./channel-DJAN_tIO.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
