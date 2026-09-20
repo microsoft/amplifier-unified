f656b005981b3682d28f3d0f6386450243e65ac3
Brian Krabach <brkrabac@microsoft.com>
2026-09-19 21:27:32 -0700

    Rebuild frontend assets for v0.11.5

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CM_iSvVV.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-DuU5ohwb.js";import{_ as a}from"./mermaid.core-APAMNJ3H.js";import"./chunk-5VM5RSS4-CtrP5jCF.js";import"./chunk-XXDRQBXY-BSp-8kpW.js";import"./chunk-POPQ4Y6H-ByUii_Wf.js";import"./chunk-F27PBJKO-1KzNU5Iq.js";import"./app-IfBoAPD9.js";import"./client-agr-qz4F.js";import"./channel-CCfOGwvA.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
