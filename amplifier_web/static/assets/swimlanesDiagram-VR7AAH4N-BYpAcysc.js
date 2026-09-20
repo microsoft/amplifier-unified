51dbb8eef948ff635674187fcf70112ee4620fda
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:50:17 -0700

    Build combined work features through connectors and worktrees

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BYpAcysc.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-CIvnpoD4.js";import{_ as a}from"./mermaid.core-tmiKTsI-.js";import"./chunk-5VM5RSS4-D1Bzx_G3.js";import"./chunk-XXDRQBXY-DmkkJ3U8.js";import"./chunk-POPQ4Y6H-BcGgF5bD.js";import"./chunk-F27PBJKO-w4gWvZsw.js";import"./app-CPC2PiNL.js";import"./client-BPXDbY79.js";import"./channel-BjlxYOMT.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
