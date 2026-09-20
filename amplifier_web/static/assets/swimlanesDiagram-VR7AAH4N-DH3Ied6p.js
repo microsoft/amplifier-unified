8dc99d183345590391ba45ef1ea0dc68256056bc
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 01:18:09 -0700

    Fix isolated provider probe startup and release 0.11.10

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DH3Ied6p.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-B9IADosQ.js";import{_ as a}from"./mermaid.core-DjfN31F8.js";import"./chunk-5VM5RSS4-Da3ecO4C.js";import"./chunk-XXDRQBXY-oHqBU11Z.js";import"./chunk-POPQ4Y6H-CRIRXD_c.js";import"./chunk-F27PBJKO-BY4I1CRJ.js";import"./app-CzwHw15z.js";import"./client-agr-qz4F.js";import"./channel-DrdtSf8C.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
