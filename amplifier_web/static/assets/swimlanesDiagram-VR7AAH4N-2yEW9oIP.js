92ac1d24833572501243f962300e84ed5819fbb8
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 16:53:11 -0700

    Build shared operation submission controls

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-2yEW9oIP.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-rpLcvImT.js";import{_ as a}from"./mermaid.core-ZOHA55Ea.js";import"./chunk-5VM5RSS4-C1qEKuuC.js";import"./chunk-XXDRQBXY-CPUydwWx.js";import"./chunk-POPQ4Y6H-uC3-9v6s.js";import"./chunk-F27PBJKO-CZA-wjNh.js";import"./app-CeRCP2_c.js";import"./client-BPXDbY79.js";import"./channel-OmC5VQlQ.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
