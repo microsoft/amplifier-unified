a7f7d8fd102ecfe063a4fd9091c1bbbbc462fb81
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:46:55 -0700

    Build connector lifecycle settings interface

---
+++ amplifier_web/static/assets/channel-BcR7or9B.js
@@ -0,0 +1,1 @@
+import{ah as o,ai as n}from"./mermaid.core-CiS2YxVW.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CzWwLNQh.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CgK7B6HM.js";import{_ as a}from"./mermaid.core-CiS2YxVW.js";import"./chunk-5VM5RSS4-BRxMwmOG.js";import"./chunk-XXDRQBXY-C05yAIL5.js";import"./chunk-POPQ4Y6H-CzZVtCs9.js";import"./chunk-F27PBJKO-BwhnbHPL.js";import"./app-ChTfTNjF.js";import"./client-BPXDbY79.js";import"./channel-BcR7or9B.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
