e72b9afa1da62bbbe1b57e96b07abade9313915a
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:10:26 -0700

    Build scheduled work controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BgUoaqCE.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DRxhfXdQ.js";import{_ as a}from"./mermaid.core-tv0v57ah.js";import"./chunk-5VM5RSS4-CNbhA-Kb.js";import"./chunk-XXDRQBXY-Dyzhs9Ny.js";import"./chunk-POPQ4Y6H-CIzCNPH5.js";import"./chunk-F27PBJKO-ZjNktb6x.js";import"./app-BxB5E-ct.js";import"./client-BPXDbY79.js";import"./channel-cWZamVd-.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
