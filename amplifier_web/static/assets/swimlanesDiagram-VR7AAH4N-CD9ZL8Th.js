5c0429cb96bbc03a884d403657c833f55ed5aa2c
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:32:19 -0700

    Build visual voice context assets

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CD9ZL8Th.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-C9l1WRs-.js";import{_ as a}from"./mermaid.core-Bzw8r9kn.js";import"./chunk-5VM5RSS4-CAOSGDxk.js";import"./chunk-XXDRQBXY-DwIZ_pLA.js";import"./chunk-POPQ4Y6H-D0ynQksu.js";import"./chunk-F27PBJKO-BtaHjt2n.js";import"./app-BAzDTzKO.js";import"./client-BPXDbY79.js";import"./channel-HPFae676.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
