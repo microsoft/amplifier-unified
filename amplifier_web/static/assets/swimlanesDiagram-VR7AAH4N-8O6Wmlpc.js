dcb8b49aec67a08c4950e9b5c83e6a22605f966a
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:35:37 -0700

    Build managed worktree controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-8O6Wmlpc.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-D_H_tUDe.js";import{_ as a}from"./mermaid.core-BN58T8L8.js";import"./chunk-5VM5RSS4-CC4Ez6yv.js";import"./chunk-XXDRQBXY-8H_zSc3d.js";import"./chunk-POPQ4Y6H-CrXsjdkJ.js";import"./chunk-F27PBJKO-C_f5dT6R.js";import"./app-CrenruJJ.js";import"./client-BPXDbY79.js";import"./channel-C3_awCDG.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
