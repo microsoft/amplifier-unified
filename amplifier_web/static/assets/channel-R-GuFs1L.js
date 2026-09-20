866ecb4436cc0736b7e1f0d298e6ae48055fb2c3
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 13:00:12 -0700

    Build question cards into the bundled app

---
+++ amplifier_web/static/assets/channel-R-GuFs1L.js
@@ -0,0 +1,1 @@
+import{ah as o,ai as n}from"./mermaid.core-UuGhr7ee.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CGRBAQdp.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DZ3T19GA.js";import{_ as a}from"./mermaid.core-UuGhr7ee.js";import"./chunk-5VM5RSS4-D_46l7i_.js";import"./chunk-XXDRQBXY-BVs-OGwp.js";import"./chunk-POPQ4Y6H-SPU0qnhJ.js";import"./chunk-F27PBJKO-CzB-vHVj.js";import"./app-CUuvi8DA.js";import"./client-BPXDbY79.js";import"./channel-R-GuFs1L.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
