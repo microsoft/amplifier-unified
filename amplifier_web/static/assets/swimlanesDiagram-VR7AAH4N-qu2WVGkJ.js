fc408fdb4271fc6ad46b53e9d7aa46d81d62eb51
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:03:40 -0700

    Build persistent computation controls

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-qu2WVGkJ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-DPo9CWdN.js";import{_ as a}from"./mermaid.core-_kGYJ1pP.js";import"./chunk-5VM5RSS4-CV4Oyp-C.js";import"./chunk-XXDRQBXY-BRqXrcwT.js";import"./chunk-POPQ4Y6H-DtyHgefR.js";import"./chunk-F27PBJKO-C4k4FHx2.js";import"./app-mV8bCTTA.js";import"./client-BPXDbY79.js";import"./channel-CiXLcUBI.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
