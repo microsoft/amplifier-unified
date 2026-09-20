6ca6f2d9debdd005267feba257ac3323ef5a000f
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 14:37:48 -0700

    Build output library and reusable writing controls

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DStyXNcM.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CQ73BV49.js";import{_ as a}from"./mermaid.core-DUg3oG98.js";import"./chunk-5VM5RSS4-Ca2yHRcO.js";import"./chunk-XXDRQBXY-BddHSVpr.js";import"./chunk-POPQ4Y6H-AuHHYqlI.js";import"./chunk-F27PBJKO-J-bua2Dt.js";import"./app-Ca2jwjon.js";import"./client-BPXDbY79.js";import"./channel-Dy5WTDH9.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
