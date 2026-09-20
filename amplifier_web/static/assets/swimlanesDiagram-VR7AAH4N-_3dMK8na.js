18faafcff4689713bfbe47f88d712ac25782a10e
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 09:54:32 -0700

    Release composer takeover overlay as 0.15.1

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-_3dMK8na.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-C_tdeObL.js";import{_ as a}from"./mermaid.core-iHIqDOWr.js";import"./chunk-5VM5RSS4-DYfjOXQ_.js";import"./chunk-XXDRQBXY-BQzcqtYn.js";import"./chunk-POPQ4Y6H-BhYtctOH.js";import"./chunk-F27PBJKO-BnntR5KL.js";import"./app-Bi_9aSDJ.js";import"./client-agr-qz4F.js";import"./channel-DKjHb1B5.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
