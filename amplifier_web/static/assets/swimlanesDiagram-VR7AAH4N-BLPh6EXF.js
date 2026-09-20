d9f48d951febd916db0f025aba6ff9e031d2f344
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 11:58:12 -0700

    Retain native form spacing inside refresh regions

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BLPh6EXF.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-CH4Ljgbw.js";import{_ as a}from"./mermaid.core-DiL9LXWw.js";import"./chunk-5VM5RSS4-rFhE3b1i.js";import"./chunk-XXDRQBXY-L2KA8rU8.js";import"./chunk-POPQ4Y6H-BA5zUNgl.js";import"./chunk-F27PBJKO-Cq3YEojb.js";import"./app-38xFuD-K.js";import"./client-BPXDbY79.js";import"./channel-Dgy-T6M7.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
