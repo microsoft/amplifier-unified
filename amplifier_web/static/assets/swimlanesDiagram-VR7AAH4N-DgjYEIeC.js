f4521534dd2f74918960b041e75a55ae1201652d
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 19:45:41 -0700

    Prepare 0.19.16 recipe compatibility release

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DgjYEIeC.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-Dhes0kkA.js";import{_ as a}from"./mermaid.core-BWXBtpjJ.js";import"./chunk-5VM5RSS4-CgQ7COdj.js";import"./chunk-XXDRQBXY-DL5kPSCQ.js";import"./chunk-POPQ4Y6H-CYdDSult.js";import"./chunk-F27PBJKO-p0YWpD09.js";import"./app-CmGaX_hI.js";import"./client-BPXDbY79.js";import"./channel-DJWweEon.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
