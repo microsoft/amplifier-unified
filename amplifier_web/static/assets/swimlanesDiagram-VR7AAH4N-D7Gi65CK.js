e24cecb7f59dc0db469a31d18475bb9f77478015
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 20:37:31 -0700

    Rebuild 0.19.17 with verified Settings navigation correction

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-D7Gi65CK.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-D3FOF8A4.js";import{_ as a}from"./mermaid.core-DgMu5-5Q.js";import"./chunk-5VM5RSS4-B8w91np1.js";import"./chunk-XXDRQBXY-BmrpKW5q.js";import"./chunk-POPQ4Y6H-kpdhPTI4.js";import"./chunk-F27PBJKO-C_Wza9Ph.js";import"./app-DbyvcMch.js";import"./client-BPXDbY79.js";import"./channel-CuaRbMr2.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
