d3a7a268b6889919af733a2f37f2874877822013
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 21:26:54 -0700

    Make mobile workspace navigation fill the screen

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BMV6jO_9.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-C3-dV68b.js";import{_ as a}from"./mermaid.core-CeRmufR8.js";import"./chunk-5VM5RSS4-P02OPdde.js";import"./chunk-XXDRQBXY-CnIFacQ5.js";import"./chunk-POPQ4Y6H-CAl6YfbQ.js";import"./chunk-F27PBJKO-DAZgKp3B.js";import"./app-ByZ9zg46.js";import"./client-BPXDbY79.js";import"./channel-CV1HbOKf.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-D2Beu8of.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-B36OU0tZ.js";import{_ as a}from"./mermaid.core-tQ_Kninp.js";import"./chunk-5VM5RSS4-D2IUcnt5.js";import"./chunk-XXDRQBXY-CZJq3nQ7.js";import"./chunk-POPQ4Y6H-D01rvqK6.js";import"./chunk-F27PBJKO-FElPircy.js";import"./app-BOwwqyqo.js";import"./client-BPXDbY79.js";import"./channel-QJl-1bF3.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
