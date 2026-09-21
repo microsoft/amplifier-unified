ba9679710865fae9aa8af591776b2c82a91bdbd1
Brian Krabach <brkrabac@microsoft.com>
2026-09-21 03:56:37 -0700

    Qualify 0.19.19 recovery and shell updates with mobile scroll fix

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-_ZDv-5PJ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BrU1xWcV.js";import{_ as a}from"./mermaid.core-D2Tkx217.js";import"./chunk-5VM5RSS4-BjYYyS6b.js";import"./chunk-XXDRQBXY-D4wPmye2.js";import"./chunk-POPQ4Y6H-nyLeFtJq.js";import"./chunk-F27PBJKO-Cvas6Zvq.js";import"./app-ClsCKlXy.js";import"./client-BPXDbY79.js";import"./channel-C2PONst1.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
