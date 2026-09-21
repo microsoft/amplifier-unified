101397dbca119874e4a6d87bfcd27dcc9bcb7c6e
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 22:14:01 -0700

    Prioritize update controls and collapse older settings history

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-ByDBj_Mt.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-C-vWT-Yz.js";import{_ as a}from"./mermaid.core-DiRTvQAW.js";import"./chunk-5VM5RSS4-C1X9-Vzj.js";import"./chunk-XXDRQBXY-CbZjGEr6.js";import"./chunk-POPQ4Y6H-CWEkHrHX.js";import"./chunk-F27PBJKO-B0ZZN3wO.js";import"./app-B3L5O1yl.js";import"./client-BPXDbY79.js";import"./channel-CiPipxod.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
