ac9dfbce54dd01d162ce4fc11e7220ac525dfe37
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 17:44:05 -0700

    Prepare composer readiness patch release 0.19.11

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DUxB1GKP.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-BmFEhU62.js";import{_ as a}from"./mermaid.core-Cdb0WRX7.js";import"./chunk-5VM5RSS4-Cbi_8Vge.js";import"./chunk-XXDRQBXY-j_rH9dhI.js";import"./chunk-POPQ4Y6H-DRuhesI3.js";import"./chunk-F27PBJKO-BHaymKAp.js";import"./app-3fggAaoL.js";import"./client-BPXDbY79.js";import"./channel-DdlsxSET.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-ek1Mleyf.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BSeyb_4w.js";import{_ as a}from"./mermaid.core-C36o1FgE.js";import"./chunk-5VM5RSS4-C47nwAOa.js";import"./chunk-XXDRQBXY-4UBtWLHX.js";import"./chunk-POPQ4Y6H-1yjhjoSk.js";import"./chunk-F27PBJKO-BMhHgaAz.js";import"./app-CL98KMZc.js";import"./client-BPXDbY79.js";import"./channel-DOe-_hnl.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
