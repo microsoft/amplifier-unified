fa2285b14400f2237b3c9baf2c12335df336dfdc
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 18:29:20 -0700

    fix(registry): validate effective and candidate module sources

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DQjFIyZ6.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CqtyL3Gi.js";import{_ as a}from"./mermaid.core-DW9Zk6hY.js";import"./chunk-5VM5RSS4-BVapjfoP.js";import"./chunk-XXDRQBXY-BPaTheVv.js";import"./chunk-POPQ4Y6H-DNrW3L5E.js";import"./chunk-F27PBJKO-A0_wnC6-.js";import"./app-Dvos9zgd.js";import"./client-BPXDbY79.js";import"./channel-Djn96K4C.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
