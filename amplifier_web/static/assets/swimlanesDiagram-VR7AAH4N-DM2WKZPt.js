ae6502219171f5e839d9b9047a1f433f7eb3e6f2
Brian Krabach <brkrabac@microsoft.com>
2026-09-20 17:19:59 -0700

    Build final reviewed Work operation controls

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DM2WKZPt.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-rO-YmKGw.js";import{_ as a}from"./mermaid.core-uVfKXJKa.js";import"./chunk-5VM5RSS4-hw_yMfmv.js";import"./chunk-XXDRQBXY-Dha7qJiZ.js";import"./chunk-POPQ4Y6H-CB3Wtr8K.js";import"./chunk-F27PBJKO-C2quUhg2.js";import"./app-Dc6HgzMy.js";import"./client-BPXDbY79.js";import"./channel-DE1qs_Sc.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
