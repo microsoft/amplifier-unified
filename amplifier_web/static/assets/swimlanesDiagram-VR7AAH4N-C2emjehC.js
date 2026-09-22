e6ea3aea3764c317408eae0ff5c7e3bed382c7f0
Amplifier <240397093+microsoft-amplifier@users.noreply.github.com>
2026-09-21 20:16:39 -0700

    Keep Settings beside More and Canvas at the right edge

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-C2emjehC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-D1ZRcTsw.js";import{_ as a}from"./mermaid.core-CDF8G0eY.js";import"./chunk-5VM5RSS4-UVMwEVh8.js";import"./chunk-XXDRQBXY-C9CQiWlw.js";import"./chunk-POPQ4Y6H-coo-KNmP.js";import"./chunk-F27PBJKO-C7DEb5H2.js";import"./app-Cs_FtkhH.js";import"./client-BPXDbY79.js";import"./channel-DTYdlJJb.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
