abcf62b7faa22f06bd60cf6b87369ff54b986fb4
Amplifier <240397093+microsoft-amplifier@users.noreply.github.com>
2026-09-21 19:40:45 -0700

    Build tested managed chat interface and update library documentation

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DXEFIwat.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-CiGbEazg.js";import{_ as a}from"./mermaid.core-BKVJZeuJ.js";import"./chunk-5VM5RSS4-D1wcXqgd.js";import"./chunk-XXDRQBXY-CvCLnlNj.js";import"./chunk-POPQ4Y6H-wv9eOLoa.js";import"./chunk-F27PBJKO-DqhjZEig.js";import"./app-B9nkz2O7.js";import"./client-BPXDbY79.js";import"./channel-Doseu1-d.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
