104ad32c08262f853990a28d4e1bd2f36d3f7f43
Brian Krabach <brkrabac@microsoft.com>
2026-09-23 07:39:29 -0700

    Prepare release 0.20.10 with reviewed reliability fixes

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-B5aPRHdR.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-DBGxMtAb.js";import{_ as a}from"./mermaid.core-DSwKAWh0.js";import"./chunk-5VM5RSS4-M8B1anow.js";import"./chunk-XXDRQBXY-BEmjgueg.js";import"./chunk-POPQ4Y6H-Cc9MnT_x.js";import"./chunk-F27PBJKO-CQsw1ghM.js";import"./app-BlgF3hQu.js";import"./client-BPXDbY79.js";import"./channel-BFQL0zTl.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
