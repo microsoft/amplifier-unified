d7c5a3a78a8b5043d25bb5b7ba73e3abd64ec0e9
Brian Krabach <brkrabac@microsoft.com>
2026-09-23 14:47:23 -0700

    Release 0.20.15: everyday Settings and published Core binaries

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DCBEtCiG.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-o8wAJu1w.js";import{_ as a}from"./mermaid.core-DqG8f4xV.js";import"./chunk-5VM5RSS4-D51f3qR5.js";import"./chunk-XXDRQBXY-CJ3Jdihg.js";import"./chunk-POPQ4Y6H-B5ohtXh2.js";import"./chunk-F27PBJKO-CUJdHpbf.js";import"./app-C4pbr5id.js";import"./client-BPXDbY79.js";import"./channel-CBysO76i.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
