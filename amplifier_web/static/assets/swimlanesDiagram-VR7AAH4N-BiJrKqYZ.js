094df30d00f6298e325729d8d6383c4bb36cb565
Brian Krabach <brkrabac@microsoft.com>
2026-09-23 23:14:12 -0700

    Release 0.20.24 with reliable tool views, exports and voice progress (#224)

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BiJrKqYZ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-C9uhb__t.js";import{_ as a}from"./mermaid.core-B3tBuS-r.js";import"./chunk-5VM5RSS4-BRWLHhsJ.js";import"./chunk-XXDRQBXY-DHMWJAaF.js";import"./chunk-POPQ4Y6H-qQbuszfv.js";import"./chunk-F27PBJKO-CqrHHOCU.js";import"./app-BiW96I7L.js";import"./client-BPXDbY79.js";import"./channel-De4xLyJu.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
