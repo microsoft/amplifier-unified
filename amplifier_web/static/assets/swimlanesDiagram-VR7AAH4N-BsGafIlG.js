be724a828639c88ad780dbdb9953d782ffb20f1e
Brian Krabach <brkrabac@microsoft.com>
2026-09-24 07:35:37 -0700

    Release 0.20.28: restore voice and improve chat interactions

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BsGafIlG.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-CnKXM7ik.js";import{_ as a}from"./mermaid.core-BozkhOSP.js";import"./chunk-5VM5RSS4-qHkyRhLb.js";import"./chunk-XXDRQBXY-DkuO5o-R.js";import"./chunk-POPQ4Y6H-BlT4VsKf.js";import"./chunk-F27PBJKO-DlXv8cWm.js";import"./app-W-TbeQ7k.js";import"./client-BPXDbY79.js";import"./channel-D8w58DEf.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
