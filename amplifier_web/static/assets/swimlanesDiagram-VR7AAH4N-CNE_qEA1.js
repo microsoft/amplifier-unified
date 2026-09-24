c763100a02e08e229b91d3d905a7dd35cfc3a746
Brian Krabach <brkrabac@microsoft.com>
2026-09-23 21:58:15 -0700

    Preserve Canvas path disclosure and chooser return focus

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-CNE_qEA1.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-BOw0EOVG.js";import{_ as a}from"./mermaid.core-CL5r2SVq.js";import"./chunk-5VM5RSS4-B8N4QK7b.js";import"./chunk-XXDRQBXY-BL2Ro2-i.js";import"./chunk-POPQ4Y6H-C-MZ5TFz.js";import"./chunk-F27PBJKO-B9u1TIV-.js";import"./app-Dxkf0SvA.js";import"./client-BPXDbY79.js";import"./channel-BtapOQDn.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-McclzFla.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-Crbvn6Zj.js";import{_ as a}from"./mermaid.core-e_CCsS0U.js";import"./chunk-5VM5RSS4-CUKfR5Bf.js";import"./chunk-XXDRQBXY-BNpeX-1B.js";import"./chunk-POPQ4Y6H-BdErVh0g.js";import"./chunk-F27PBJKO-CR30ZRXp.js";import"./app-_dbt7k45.js";import"./client-BPXDbY79.js";import"./channel-gW24bRvi.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
