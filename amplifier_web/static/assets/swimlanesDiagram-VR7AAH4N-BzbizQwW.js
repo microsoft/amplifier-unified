f83e722b8429569a8db5caa523b031910ec0dd14
Brian Krabach <brkrabac@microsoft.com>
2026-09-24 01:44:01 -0700

    Rebuild chat assets for quoted replies and reactions

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BzbizQwW.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-Dfkxi88C.js";import{_ as a}from"./mermaid.core-LSXp7a8x.js";import"./chunk-5VM5RSS4-q23Koo6O.js";import"./chunk-XXDRQBXY-0Np_Ly14.js";import"./chunk-POPQ4Y6H-CW_MbQKO.js";import"./chunk-F27PBJKO-DDOPAxA3.js";import"./app-C20qJNph.js";import"./client-BPXDbY79.js";import"./channel-COJTY35W.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
