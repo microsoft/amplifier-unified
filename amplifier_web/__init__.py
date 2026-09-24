8611227a130778d18c4d35f2293845609068756b
Brian Krabach <brkrabac@microsoft.com>
2026-09-24 07:39:12 -0700

    Release 0.20.28: restore voice and improve chat interactions (#235)

--- amplifier_web/__init__.py
+++ amplifier_web/__init__.py
@@ -1,2 +1,2 @@
 """Amplifier's local web host."""
-__version__ = "0.20.27"
+__version__ = "0.20.28"
---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DI4bass_.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DQHzqpJq.js";import{_ as a}from"./mermaid.core-B0mTgFdj.js";import"./chunk-5VM5RSS4-tGpMiYU7.js";import"./chunk-XXDRQBXY-qr9gqTjE.js";import"./chunk-POPQ4Y6H-DfYEExkB.js";import"./chunk-F27PBJKO-_s8idNc1.js";import"./app-Cr2ht-w5.js";import"./client-BPXDbY79.js";import"./channel-BE-C4Qfn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
