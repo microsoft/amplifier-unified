8473d520a167fdec8e0f0834189ad97a5f1f38b2
Brian Krabach <brkrabac@microsoft.com>
2026-09-23 17:44:34 -0700

    Release 0.20.17: Settings setup and update progress (#200)

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-BpIsUl6A.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-BDSgamvM.js";import{_ as a}from"./mermaid.core-PsFdDvWx.js";import"./chunk-5VM5RSS4-D8AlHn48.js";import"./chunk-XXDRQBXY-CchI7IGG.js";import"./chunk-POPQ4Y6H-CILtqt8m.js";import"./chunk-F27PBJKO-CINMVysa.js";import"./app-OmXgDyCS.js";import"./client-BPXDbY79.js";import"./channel-BLCE-6PS.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
