c2c35e93eaf724a9417e99dcb1ec7bcf12756321
Brian Krabach <brkrabac@microsoft.com>
2026-09-24 01:48:49 -0700

    Release 0.20.27 with reliable workspace switching (#233)

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-Aj1l3DCi.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DTDwS5zX.js";import{_ as a}from"./mermaid.core-Cd_Qm2ev.js";import"./chunk-5VM5RSS4-CcWKUV6r.js";import"./chunk-XXDRQBXY-DbY9Uugs.js";import"./chunk-POPQ4Y6H-DVv_hfou.js";import"./chunk-F27PBJKO-CiSkxRLu.js";import"./app-BsnuAaMw.js";import"./client-BPXDbY79.js";import"./channel-BcIvRPIj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
