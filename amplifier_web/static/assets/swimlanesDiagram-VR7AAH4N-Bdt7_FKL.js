642347d00ae0147246c201f93e8e0ae71be2f2ce
Brian Krabach <brkrabac@microsoft.com>
2026-09-25 03:51:10 -0700

    Keep call conversation work stoppable during voice

--- amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-Bdt7_FKL.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-BHoQJDlm.js";import{_ as a}from"./mermaid.core-J6bMCGG4.js";import"./chunk-5VM5RSS4-B3dbugFd.js";import"./chunk-XXDRQBXY-txVQWD0x.js";import"./chunk-POPQ4Y6H-CRIsBgYD.js";import"./chunk-F27PBJKO-DDYVUjmW.js";import"./app-DFumaEn3.js";import"./client-BPXDbY79.js";import"./channel-C7aKgtsw.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
