a06f06b9de7379e7b5c19c1a873452c5fe3b886e
Brian Krabach <brkrabac@microsoft.com>
2026-09-25 22:28:28 -0700

    Prepare 0.20.32 settings performance release assets

---
+++ amplifier_web/static/assets/swimlanesDiagram-VR7AAH4N-DjTuSUZe.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CScf9EJq.js";import{_ as a}from"./mermaid.core-C7rteDFv.js";import"./chunk-5VM5RSS4-fC6PNziR.js";import"./chunk-XXDRQBXY-B0-WRkom.js";import"./chunk-POPQ4Y6H-41W0Cd2B.js";import"./chunk-F27PBJKO-B_1ULT_2.js";import"./app-BUYIxJoo.js";import"./client-BPXDbY79.js";import"./channel-BtljQf1j.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
